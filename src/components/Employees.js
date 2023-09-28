4d025b0e0cbe6656ff3ab8b86df1a9a20608d680
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-09-28 23:37:42 +0530

    initial commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Aman Deshwal",
+        Age:"30"
+    },
+    {
+        id:"2",
+        Name:"Vivek Atri",
+        Age:"28"
+    },
+    {
+        id:"3",
+        Name:"Sahil Rathi",
+        Age:"29"
+    },
+]
+
+export default Employees;
